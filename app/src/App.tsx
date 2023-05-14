bebf5d49e149d99b69550439fa2a2b65f5e0f0ee
juandapps <juandapps@hotmail.com>
2023-05-14 14:55:05 -0500

    📝TypeScript Aliases

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,46 +2,31 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  //   Types
-  // let name: string;
-  // let age: number;
-  // let isStudent: boolean;
-  // let hobbies: string[];
-  // let studies: number[];
-  // let role: [number, string]; // tuples contains one of each
-  // role = [5, "name"];
+  // aliases: types-and-interface
 
-  // Objects
-  // type Person = {
-  //   name: string;
-  //   age?: number; // ? optional property
+  interface Person {
+    name: string;
+    age?: number;
+  }
+
+  interface Guy extends Person {
+    profession: string;
+  }
+
+  type X = {
+    a: string;
+    b: number;
+  };
+
+  // type Y = X & {
+  //   c: string;
+  //   d: number;
   // };
-  //
-  // let person: Person = {
-  //   name: "DRT",
-  //   age: 24,
-  // };
-  //
-  // console.log(person);
-  //
-  // let lotsOfPeople: Person[];
 
-  // let age: number | string;
-  // age = 24; // "String" or 24
-  // console.log(age);
-  //
-  // function printName(name: string) {
-  //   console.log(name);
-  // }
-  //
-  // printName("DRT");
-
-  // let printName: Function;
-
-  // let printName: (name: string) => void; // void returns undefined
-  // let printName: (name: string) => never; // never returns nothing
-  // let name: any; // not recommended, why would we need ts then??
-  // let personName: unknown; // recommended instead of any if you don't know what type is going to be.
+  let y: Y = {
+    c: "efdas",
+    d: 42,
+  };
 
   return <div className="App">Hello World</div>;
 }
