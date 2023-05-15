c231126cd97c1d4d2e1bb5cbabf8554f43d105c8
juandapps <juandapps@hotmail.com>
2023-05-14 20:34:18 -0500

    ➕Add TodoList component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
 import { Todo } from "./model";
+import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
@@ -22,7 +23,7 @@ const App: React.FC = () => {
     <div className="App">
       <span className="heading">Taskify</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-      {/*<TodoList />*/}
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 };
